78455c129264fb733f91d9d82c11efaa1f823fff
mirza-sync <mirzasyahmi123@gmail.com>
2024-04-06 23:57:02 +0800

    Make frame drawer shows dynamic animation

--- script.js
+++ script.js
@@ -7,18 +7,48 @@ const playerImage = new Image()
 playerImage.src = 'shadow_dog.png'
 const spriteWidth = 575
 const spriteHeight = 523
-let frameX = 0
-let frameY = 0
 let gameFrame = 0
 const staggerFrames = 5
+const spriteAnimations = []
+const animationStates = [
+  {
+    name: 'idle',
+    frames: 7,
+  },
+  {
+    name: 'jump',
+    frames: 7,
+  },
+  {
+    name: 'land',
+    frames: 7,
+  },
+]
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: []
+  }
+
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth
+    let positionY = index * spriteHeight
+    frames.loc.push({ x: positionX, y: positionY })
+  }
+
+  spriteAnimations[state.name] = frames
+})
+console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
   // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
-  let position = Math.floor(gameFrame / staggerFrames) % 6
-  frameX = spriteWidth * position
+  let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations['idle'].loc.length
+  let frameX = spriteAnimations['idle'].loc[position].x
+  let frameY = spriteAnimations['idle'].loc[position].y
   ctx.drawImage(playerImage,
-    frameX, frameY * spriteHeight, spriteWidth, spriteHeight,
+    frameX, frameY,
+    spriteWidth, spriteHeight,
     0, 0, spriteWidth, spriteHeight
   )
 
