f59ecb96d8d554245fae93a0f18c5a23f01d2512
mirza-sync <mirzasyahmi123@gmail.com>
2024-04-06 18:23:21 +0800

    Show a dog

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext("2d")
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 600
+
+const playerImage = new Image()
+playerImage.src = 'shadow_dog.png'
+const spriteWidth = 575
+const spriteHeight = 523
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+  ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+  requestAnimationFrame(animate)
+}
+animate()
