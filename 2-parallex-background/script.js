350fe30ee9f429c084f529a8235b376618a2a3f0
mirza-sync <mirzasyahmi123@gmail.com>
2024-04-07 22:40:11 +0800

    Add canvas

--- 2-parallex-background/script.js
+++ 2-parallex-background/script.js
@@ -1,1 +1,5 @@
-alert("Hello")
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+const CANVAS_WIDTH = canvas.width = 800
+const CANVAS_HEIGHT = canvas.height = 700
+let gameSpeed = 5
