8230d69c1ca6e8c721498f62c061368b183f0abe
mirza-sync <mirzasyahmi123@gmail.com>
2024-04-08 11:05:23 +0800

    Add background images

--- 2-parallex-background/script.js
+++ 2-parallex-background/script.js
@@ -2,4 +2,34 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
-let gameSpeed = 5
+let gameSpeed = 50
+
+const backgroundLayer1 = new Image()
+backgroundLayer1.src = 'assets/layer-1.png'
+
+const backgroundLayer2 = new Image()
+backgroundLayer2.src = 'assets/layer-2.png'
+
+const backgroundLayer3 = new Image()
+backgroundLayer3.src = 'assets/layer-3.png'
+
+const backgroundLayer4 = new Image()
+backgroundLayer4.src = 'assets/layer-4.png'
+
+const backgroundLayer5 = new Image()
+backgroundLayer5.src = 'assets/layer-5.png'
+
+let x = 0
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  ctx.drawImage(backgroundLayer4, x, 0)
+  if (x < -2400) {
+    x = 0
+  } else {
+    x -= gameSpeed
+  }
+  requestAnimationFrame(animate)
+}
+
+animate()
