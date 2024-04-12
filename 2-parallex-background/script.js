2e50376f5e82d913ac3c50c16857696804e3eb81
mirza-sync <mirzasyahmi123@gmail.com>
2024-04-13 00:13:18 +0800

    Add continuous background

--- 2-parallex-background/script.js
+++ 2-parallex-background/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
-let gameSpeed = 50
+let gameSpeed = 15
 
 const backgroundLayer1 = new Image()
 backgroundLayer1.src = 'assets/layer-1.png'
@@ -20,14 +20,22 @@ const backgroundLayer5 = new Image()
 backgroundLayer5.src = 'assets/layer-5.png'
 
 let x = 0
+let x2 = 2400
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  // drawImage(image, dx, dy)
   ctx.drawImage(backgroundLayer4, x, 0)
+  ctx.drawImage(backgroundLayer4, x2, 0)
   if (x < -2400) {
-    x = 0
+    x = 2400 + x2 - gameSpeed
   } else {
     x -= gameSpeed
+  }
+  if (x2 < -2400) {
+    x2 = 2400 + x - gameSpeed
+  } else {
+    x2 -= gameSpeed
   }
   requestAnimationFrame(animate)
 }
