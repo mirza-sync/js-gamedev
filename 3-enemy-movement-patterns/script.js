7625637d1f2878e59e1ae02ac4984b1af532683c
mirza-sync <mirzasyahmi123@gmail.com>
2024-04-13 04:15:24 +0800

    Generate 100 random enemies

--- 3-enemy-movement-patterns/script.js
+++ 3-enemy-movement-patterns/script.js
@@ -2,18 +2,21 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 500
 const CANVAS_HEIGHT = canvas.height = 600
+const numberOfEnemies = 100
+let enemiesArray = []
 
 class Enemy {
   constructor() {
-    this.x = 0
-    this.y = 0
+    this.x = Math.random() * CANVAS_WIDTH
+    this.y = Math.random() * CANVAS_HEIGHT
     this.width = 100
     this.height = 100
+    this.speed = (Math.random() * 4) - 2 // Generate random numbers between -2 and +2
   }
 
   update() {
-    this.x++
-    this.y++
+    this.x += this.speed
+    this.y += this.speed
   }
 
   draw() {
@@ -21,12 +24,16 @@ class Enemy {
   }
 }
 
-const enemy1 = new Enemy()
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy())
+}
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  enemy1.update()
-  enemy1.draw()
+  enemiesArray.forEach(enemy => {
+    enemy.update()
+    enemy.draw()
+  })
   requestAnimationFrame(animate)
 }
 
