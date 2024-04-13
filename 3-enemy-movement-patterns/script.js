fca62506f440b7bf221763b693676575319afc6c
mirza-sync <mirzasyahmi123@gmail.com>
2024-04-13 23:51:52 +0800

    Animate enemy1

--- 3-enemy-movement-patterns/script.js
+++ 3-enemy-movement-patterns/script.js
@@ -5,6 +5,7 @@ const CANVAS_WIDTH = canvas.width = 500
 const CANVAS_HEIGHT = canvas.height = 600
 const numberOfEnemies = 100
 let enemiesArray = []
+let gameFrame = 0
 
 const enemyImage = new Image()
 enemyImage.src = 'assets/enemy1.png'
@@ -13,18 +14,26 @@ class Enemy {
   constructor() {
     this.x = Math.random() * CANVAS_WIDTH
     this.y = Math.random() * CANVAS_HEIGHT
-    this.width = 100
-    this.height = 100
+    this.spriteWidth = 293
+    this.spriteHeight = 155
+    this.width = this.spriteWidth / 2.5
+    this.height = this.spriteHeight / 2.5
     this.speed = (Math.random() * 4) - 2 // Generate random numbers between -2 and +2
+    this.frame = 0
   }
 
   update() {
     this.x += this.speed
     this.y += this.speed
+    // animate sprites
+    if (gameFrame % 2 === 0) {
+      this.frame > 4 ? this.frame = 0 : this.frame++
+    }
   }
 
   draw() {
-    ctx.drawImage(enemyImage, this.x, this.y)
+    ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,
+      this.x, this.y, this.width, this.height)
   }
 }
 
@@ -38,6 +47,7 @@ function animate() {
     enemy.update()
     enemy.draw()
   })
+  gameFrame++
   requestAnimationFrame(animate)
 }
 
